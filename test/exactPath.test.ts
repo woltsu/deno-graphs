842671789545e4e9d053e59c7718f9e3f22c3619
Rochet2 <tqbattlenet@gmail.com>
2020-10-04 01:50:27 +0300

    Fix the test logic

--- test/exactPath.test.ts
+++ test/exactPath.test.ts
@@ -36,10 +36,10 @@ Deno.test("exactPath - no path", () => {
   s.add(b, 0);
   s.add(c, 0);
   b.add(c, 1);
-  b.add(t, 0);
+  c.add(b, 2);
   G.add(s, b, c, t);
 
-  assertEquals(exactPath(G, s, t, 0), false);
+  assertEquals(exactPath(G, s, t, 0), true);
   assertEquals(exactPath(G, s, t, 1), false);
   assertEquals(exactPath(G, s, t, 2), false);
   assertEquals(exactPath(G, s, t, 3), false);
